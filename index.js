18f99526337dd12ee853a8f246f7a60f79754869
snehaChaudhari <37608348+snehaChaudhari@users.noreply.github.com>
2019-12-22 15:39:54 +0530

    add to fav, search func.

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
-/**
- * @format
- */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
-
-
 
 import React,{ Component } from 'react'
 import {AppRegistry} from 'react-native';
-// import App from './App';
 import Navigator from './src/Router/StackNav'
 import {name as appName} from './app.json';
 import { Provider } from 'react-redux';
